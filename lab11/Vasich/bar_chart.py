5ce933497ca6c4cd1ae87c71cad25bf2afe76470
graystrappado <igelfanger@gmail.com>
2016-01-11 13:49:31 +0300

    lab 11 - repetitions removed

--- lab11/Vasich/bar_chart.py
+++ lab11/Vasich/bar_chart.py
@@ -1,33 +1,24 @@
+def one_pass_sum(seq, indices):
+    max_sum = float("-inf")
+    curr_sum = 0
+    height = seq[indices[0]]
+
+    for i in indices[1:]:
+        if seq[i] < height:
+            curr_sum += height - seq[i]
+        else:
+            if curr_sum >= max_sum:
+                max_sum = curr_sum
+            height = seq[i]
+            curr_sum = 0
+    return max_sum
+
 
 def chart_area(seq):
     if len(seq) < 3:
         return 0
 
-    max_sum = float("-inf")
+    forward = one_pass_sum(seq, range(len(seq)))
+    backward = one_pass_sum(seq, range(len(seq) - 1, -1, -1))
 
-    height = seq[0]
-    curr_sum = 0
-
-    for i in range(1, len(seq)):
-        if seq[i] < height:
-            curr_sum += height - seq[i]
-        else:
-            if curr_sum >= max_sum:
-                max_sum = curr_sum
-            height = seq[i]
-            curr_sum = 0
-
-    height = seq[len(seq) - 1]
-    curr_sum = 0
-
-    for i in range(len(seq) - 2, -1, -1):
-        if seq[i] < height:
-            curr_sum += height - seq[i]
-        else:
-            if curr_sum >= max_sum:
-                max_sum = curr_sum
-            height = seq[i]
-            curr_sum = 0
-
-    return max_sum
-
+    return max(forward, backward)
