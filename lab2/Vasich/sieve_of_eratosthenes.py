e0a859ad79d56fb90bf6d291df45ade125bfd8ee
graystrappado <graystrappado@gmail.com>
2016-04-22 03:23:23 +0300

    faster version of the sieve

--- lab2/Vasich/sieve_of_eratosthenes.py
+++ lab2/Vasich/sieve_of_eratosthenes.py
@@ -1,3 +1,26 @@
+from math import sqrt, floor
+from itertools import chain
+
+#faster version
+def sieve_sets(n):
+    if n > 3:
+        yield from (2, 3)
+    elif n == 3:
+        yield 2
+    else:
+        return
+
+    primes = set(chain(range(5, n, 6), range(7, n, 6)))
+    stop, v = floor(sqrt(n)), 5
+    while v <= stop:
+        yield v
+        primes.remove(v)
+        for p in range(v * v, n, 2 * v):
+            primes.discard(p)
+        v = min(primes)
+    yield from sorted(primes)
+    
+    
 def sieve(n):
     primes = [m % 2 == 1 for m in range(n + 1)]
     primes[1] = False
